d1def5de2a91f08c567201a898f47ce356b788b2
apurbanaskarcse <apurbanaskarcse@outlook.com>
2023-06-11 20:57:22 +0530

    first comit

---
+++ MailSendDemo/src/mailsenddemo/MailSendDemo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mailsenddemo;
+
+/**
+ *
+ * @author Deepak
+ */
+public class MailSendDemo
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            TestSendMail.sendMail("to_email_id@gmail.com");
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+}
